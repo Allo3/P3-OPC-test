f32a11eb2eae5c1b715eb57d2c700744421893f6
Allo3 <allo.louchet@gmail.com>
2023-03-23 20:51:36 +0100

    login done
    js pour la connexion finie
    
    next modif page quand admin

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,16 +1,46 @@
-let user = {
-    "email": "string",
-    "password": "string"
-};
+const loginForm = document.querySelector("#login-form");
+const connectButton = document.querySelector("#connect");
+const emailInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const errorMessage = document.querySelector("#error-message");
 
-let userEntry = awaitfetch("http://localhost:5678/api/users/login", {
-    method: 'POST',
+
+connectButton.addEventListener("click", async (event) => {
+  event.preventDefault();
+
+  // Vérifie que les deux champs sont remplis
+  if (!emailInput.value || !passwordInput.value) {
+    errorMessage.textContent = "Veuillez remplir tous les champs";
+    console.log("Les champs ne sont pas remplis");
+    return;
+  }
+
+  // Envoie la requête POST pour la connexion
+  const reponse = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
     headers: {
-        'Content-Type': application.json
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(userEntry)
+    body: JSON.stringify({
+      email: emailInput.value,
+      password: passwordInput.value
+    })
+  });
+
+  // Si la réponse est OK, stocke les informations dans le local storage
+  if (reponse.ok) {
+    const data = await reponse.json();
+    localStorage.setItem("userId", data.userId);
+    localStorage.setItem("token", data.token);
+    console.log("Connexion réussie");
+    window.location.replace("index.html");
+  } else if (reponse.status === 401) {
+    // Si le mot de passe est incorrect, affiche un message d'erreur
+    errorMessage.textContent = "Mot de passe incorrect";
+    console.log("Mot de passe incorrect");
+  } else {
+    // Si l'utilisateur ou le mot de passe est incorrect, affiche un message d'erreur
+    errorMessage.textContent = "Utilisateur ou mot de passe incorrect";
+    console.log("Utilisateur ou mot de passe incorrect");
+  }
 });
-
-let result = await userEntry.json();
-alert(result.message);
-
--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -5,25 +5,21 @@ let divGallery = document.querySelector("#gallery");
 
 const allBouton = document.getElementById('all');
 allBouton.addEventListener("click", () => {
-    console.log("all");
     removeProjects();
     displayProjects(projets);
 })
 const objetsBouton = document.getElementById('objets');
 objetsBouton.addEventListener("click", () => {
-    console.log("1");
     removeProjects();
     displayProjects(projets.filter((projet) => projet.categoryId === 1));
 })
 const appartBouton = document.getElementById('appart');
 appartBouton.addEventListener("click", () => {
-    console.log("2");
     removeProjects();
     displayProjects(projets.filter((projet) => projet.categoryId === 2));
 })
 const hotelRestaurantBouton = document.getElementById('hotel-resto');
 hotelRestaurantBouton.addEventListener("click", () => {
-    console.log("3");
     removeProjects();
     displayProjects(projets.filter((projet) => projet.categoryId === 3));
 
@@ -55,8 +51,6 @@ function removeProjects() {
 
     const list = document.getElementById("gallery");
     while (list.children.length > 0) {
-        console.log('list.children.length', list.children.length);
-        console.log('list.hasChildNodes()', list.hasChildNodes());
         list.removeChild(list.firstElementChild);
     }
 }
